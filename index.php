c10254c6c0a71d6d89df568b8ce081ce7a225a05
Daniel Stoops <contact@danielstoops.com>
2016-06-09 20:28:00 -0500

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-wow
+		<div class="outer">
+			<div class="middle">
+				<div class="inner">
+					<h1 id="login">partyzone</h1>
+					<div id="hr">
+						<hr class="hr1"></hr>
+					</div>
+					<div id="button">
+						<button type="submit">Log in</button>
+					</div>
+				</div>
+			</div>
+		</div>
